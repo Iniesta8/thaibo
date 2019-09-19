b3235b916b5299a9193e0bcf35872c10cd4f8944
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2019-09-19 19:34:17 +0200

    Add simple impl

---
+++ thaibo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+import requests
+
+source = requests.get("https://www.yim-yim.de/speisen/mittags-abo/").text
+
+soup = BeautifulSoup(source, "lxml")
+
+print('\n' + soup.h2.text + '\n')
+
+print("Gültig: " + soup.strong.text + " (" + soup.find("div", class_="content speisekarte").p.text + ")\n")
+
+for starter in soup.find_all("p", class_="speisenname"):
+    print(starter.text)
+
+print('\n' + "Hauptspeisen:" '\n')
+
+for main_course in soup.find_all("span", class_="speisenname"):
+    parent = main_course.find_parent("p")
+    print("- " + parent.find(class_="speisenname").text +
+          " (" + parent.find(class_="speisenbeschreibung").text.strip() +
+          "): " + parent.find(class_="preis").text.strip())
+
